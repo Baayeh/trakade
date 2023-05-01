309791c52266c7754d88c384d4f560d093609f32
strangeal <bajerijustice@gmail.com>
2023-05-01 14:39:11 +0000

    Modify category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
-  has_many :items
-  has_many :order_items, through: :items
+  has_many :items, dependent: :destroy
+  has_many :order_items, through: :items, dependent: :destroy
 end
