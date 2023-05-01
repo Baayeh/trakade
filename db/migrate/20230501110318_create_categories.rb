2d3a760dfd7a41f53f015e5c33d97783ef0df3f9
strangeal <bajerijustice@gmail.com>
2023-05-01 11:04:30 +0000

    Generate category model and table

---
+++ db/migrate/20230501110318_create_categories.rb
@@ -0,0 +1,12 @@
+class CreateCategories < ActiveRecord::Migration[7.0]
+  def change
+    create_table :categories do |t|
+      t.string :name
+      t.string :sku
+      t.decimal :price
+      t.integer :quantity
+
+      t.timestamps
+    end
+  end
+end
